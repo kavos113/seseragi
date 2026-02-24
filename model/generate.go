043b8c877b1325613adcf452b11d7707dfd0397f
kavos113 <kohei.hadano@gmail.com>
2026-02-24 17:42:34 +0900

    fix mock path

--- model/generate.go
+++ model/generate.go
@@ -1,4 +1,4 @@
 package model
 
-//go:generate mockgen -source=task.go -destination=./mock/task_mock.go -package=mock_model
-//go:generate mockgen -source=workflow.go -destination=./mock/workflow_mock.go -package=mock_model
+//go:generate mockgen -source=task.go -destination=./mock_model/task_mock.go -package=mock_model
+//go:generate mockgen -source=workflow.go -destination=./mock_model/workflow_mock.go -package=mock_model
