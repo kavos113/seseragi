49730a5bfd45c3ee416f1604eccf014d2c9cbfb8
kavos113 <kohei.hadano@gmail.com>
2026-02-24 17:09:48 +0900

    mockgen

---
+++ model/generate.go
@@ -0,0 +1,4 @@
+package model
+
+//go:generate mockgen -source=task.go -destination=./mock/task_mock.go -package=mock_model
+//go:generate mockgen -source=workflow.go -destination=./mock/workflow_mock.go -package=mock_model
--- model/go.mod
+++ model/go.mod
@@ -1,3 +1,5 @@
 module github.com/kavos113/seseragi/model
 
 go 1.24.4
+
+require go.uber.org/mock v0.6.0
