55146da4ef42e7275b6712ef393fc76b02b2aa69
kavos113 <kohei.hadano@gmail.com>
2026-02-25 09:22:05 +0900

    go sdk

---
+++ sdk/golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/kavos113/seseragi/sdk/golang
+
+go 1.25.1
---
+++ sdk/golang/seseragi.go
@@ -0,0 +1,49 @@
+package golang
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func Run[I any, O any](handler func(I) (O, error)) {
+	inPath := os.Getenv("WORKFLOW_INPUT_PATH")
+	outPath := os.Getenv("WORKFLOW_OUTPUT_PATH")
+
+	inData, err := os.ReadFile(inPath)
+	if err != nil {
+		writeError(outPath, err)
+		os.Exit(1)
+	}
+
+	var input I
+	err = json.Unmarshal(inData, &input)
+	if err != nil {
+		writeError(outPath, err)
+		os.Exit(1)
+	}
+
+	output, err := handler(input)
+	if err != nil {
+		writeError(outPath, err)
+		os.Exit(1)
+	}
+
+	outData, err := json.Marshal(output)
+	if err != nil {
+		writeError(outPath, err)
+		os.Exit(1)
+	}
+
+	err = os.WriteFile(outPath, outData, 0644)
+	if err != nil {
+		writeError(outPath, err)
+		os.Exit(1)
+	}
+}
+
+func writeError(path string, err error) {
+	errData, _ := json.Marshal(map[string]string{
+		"error": err.Error(),
+	})
+	os.WriteFile(path, errData, 0644)
+}
