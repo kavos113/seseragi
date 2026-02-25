8902165f8f3bd99f843434b64927f835b9f59fbb
kavos113 <kohei.hadano@gmail.com>
2026-02-25 15:59:16 +0900

    rename mock

--- internal/domain/generate.go
+++ internal/domain/generate.go
@@ -1,4 +1,4 @@
 package domain
 
-//go:generate mockgen -source=task.go -destination=./mock_model/task_mock.go -package=mock_model
-//go:generate mockgen -source=workflow.go -destination=./mock_model/workflow_mock.go -package=mock_model
+//go:generate mockgen -source=task.go -destination=./mock_domain/task_mock.go -package=mock_domain
+//go:generate mockgen -source=workflow.go -destination=./mock_domain/workflow_mock.go -package=mock_domain
---
+++ internal/usecase/task_test.go
@@ -0,0 +1,20 @@
+package usecase
+
+import (
+	"testing"
+
+	"github.com/kavos113/seseragi/internal/domain"
+)
+
+func TestAddTask(t *testing.T) {
+	tests := []struct {
+		name string
+		setupMock func(repo *)
+		task domain.Task
+		wantErr bool
+	}{
+		{
+
+		}
+	}
+}
