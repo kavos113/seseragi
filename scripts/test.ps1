9a949f48f482b5539b5c18850b307e5446f1b4ac
kavos113 <kohei.hadano@gmail.com>
2026-02-25 18:58:32 +0900

    test simple

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,11 +2,6 @@ Param(
     [string]$cover
 )
 
-$testableDir = @(
-    "manager",
-    "model"
-)
-
 if (Test-Path "cover") {
     Remove-Item "cover" -Recurse -Force
 }
@@ -15,22 +10,13 @@ $coverageDir = Join-Path (Get-Location) "cover"
 
 $getCover = $cover -ne $null -and $cover -eq "cover"
 
-foreach ($dir in $testableDir) {
-    Write-Host "-------- Running tests in $dir ------------" -ForegroundColor Cyan
-    Push-Location $dir
-    try {
-        if ($getCover) {
-            $coverFile = Join-Path $coverageDir "$dir.coverprofile"
-            $htmlFile = Join-Path $coverageDir "$dir.coverage.html"
+if ($getCover) {
+    $coverFile = Join-Path $coverageDir "$dir.coverprofile"
+    $htmlFile = Join-Path $coverageDir "$dir.coverage.html"
 
-            go test ./... -v -covermode=atomic -coverprofile="$coverFile"
-            go tool cover -html="$coverFile" -o "$htmlFile"
-        }
-        else {
-            go test ./... -v
-        }
-    }
-    finally {
-        Pop-Location
-    }
+    go test ./... -v -covermode=atomic -coverprofile="$coverFile"
+    go tool cover -html="$coverFile" -o "$htmlFile"
+}
+else {
+    go test ./... -v
 }
