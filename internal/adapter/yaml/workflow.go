df31477d7a4a40d3c60560f0c6fe8cc2e9a81036
kavos113 <kohei.hadano@gmail.com>
2026-02-26 09:42:15 +0900

    parse interval

--- internal/adapter/yaml/workflow.go
+++ internal/adapter/yaml/workflow.go
@@ -1,6 +1,8 @@
 package yaml
 
 import (
+	"time"
+
 	"github.com/kavos113/seseragi/internal/domain"
 	"go.yaml.in/yaml/v4"
 )
@@ -10,10 +12,16 @@ type nodeInfo struct {
 	Dependencies []string `yaml:"dependencies"`
 }
 
+type triggerInfo struct {
+	Type     string `yaml:"type"`
+	Interval int    `yaml:"interval,omitempty"` // minutes
+}
+
 type workflowInfo struct {
 	Name        string              `yaml:"name"`
 	Description string              `yaml:"description"`
 	Nodes       map[string]nodeInfo `yaml:"nodes"`
+	Trigger     triggerInfo         `yaml:"trigger"`
 }
 
 // return ID, is empty
@@ -33,9 +41,17 @@ func LoadWorkflowInfoFromYAML(yamlData []byte, yamlPath string) (*domain.Workflo
 		})
 	}
 
-	return &domain.Workflow{
+	workflow := &domain.Workflow{
 		Name:     workflowInfo.Name,
 		Nodes:    nodes,
 		YamlPath: yamlPath,
-	}, nil
+	}
+
+	if workflowInfo.Trigger.Type == "interval" {
+		workflow.RunInterval = time.Duration(workflowInfo.Trigger.Interval) * time.Minute
+	} else {
+		workflow.RunInterval = time.Duration(time.Hour * 24 * 365 * 100) // effectively never run
+	}
+
+	return workflow, nil
 }
