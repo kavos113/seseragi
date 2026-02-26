2d9ac11652579b91cf042f86c96f7985e7cc3af5
kavos113 <kohei.hadano@gmail.com>
2026-02-26 14:26:31 +0900

    ignore

--- internal/adapter/docker/image.go
+++ internal/adapter/docker/image.go
@@ -5,20 +5,90 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/moby/moby/client"
 )
 
 func (c *Client) BuildImage(imageName string, contextDir string) error {
-	var tarBuf bytes.Buffer
-	tw := tar.NewWriter(&tarBuf)
+	var excludes []string
 
-	if err := tw.AddFS(os.DirFS(contextDir)); err != nil {
+	ignorePath := filepath.Join(contextDir, ".dockerignore")
+	f, err := os.Open(ignorePath)
+	if err == nil {
+		defer f.Close()
+
+		content, err := io.ReadAll(f)
+		if err != nil {
+			return err
+		}
+
+		line := strings.Split(string(content), "\n")
+		for _, l := range line {
+			l = strings.TrimSpace(l)
+			if l != "" {
+				excludes = append(excludes, l)
+			}
+		}
+	} else if !os.IsNotExist(err) {
 		return err
 	}
 
-	err := tw.Close()
+	var tarBuf bytes.Buffer
+	tw := tar.NewWriter(&tarBuf)
+
+	err = filepath.Walk(contextDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(contextDir, path)
+		if err != nil {
+			return err
+		}
+
+		for _, pattern := range excludes {
+			if strings.Contains(relPath, pattern) {
+				return nil
+			}
+		}
+
+		header := &tar.Header{
+			Name:    relPath,
+			Size:    info.Size(),
+			Mode:    int64(info.Mode()),
+			ModTime: info.ModTime(),
+		}
+
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		if _, err := io.Copy(tw, file); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	err = tw.Close()
 	if err != nil {
 		return err
 	}
